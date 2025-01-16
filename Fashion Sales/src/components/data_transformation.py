d82a546bd09e70729599f49ee82eabb04fb7ef8e
Santiq22 <luciopoto@gmail.com>
2025-01-16 15:19:16 -0300

    Data transformation started

--- Fashion Sales/src/components/data_transformation.py
+++ Fashion Sales/src/components/data_transformation.py
@@ -5,3 +5,26 @@ Created on Wed Sep 25 23:36:11 2024
 @author: Santiago Collazo
 @original_author: Krish Naik
 """
+# ========================================= Packages ============================================ #
+import os
+import sys
+sys.path.insert(0, '/mnt/c/Programacion/Data Science Projects/Fashion Sales/src')
+from exception import CustomException
+from logger import logging
+
+from dataclasses import dataclass
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+# =============================================================================================== #
+
+# ======================================== Main classes ========================================= #
+# This class will provide the paths for the inputs to the data transformation process
+@dataclass
+class DataTransformationConfig:
+    # Preprocessor object filepath
+    preprocessor_obj_file_path = os.path.join('data', 'preprocessor.pkl')
+# =============================================================================================== #
